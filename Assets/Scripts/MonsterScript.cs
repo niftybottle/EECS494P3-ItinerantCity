7393b114db2e39db70faacd8e0168f7c29859cb2
niftybottle <niftybottle@gmail.com>
2014-03-23 23:16:12 -0400

    Moving monsters

---
+++ Assets/Scripts/MonsterScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MonsterScript : MonoBehaviour {
+	public GameObject go;
+	public float speed;
+	// Use this for initialization
+	void Start () {
+		go = GameObject.Find("Castle");
+		speed = .5f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(go.transform.position.x > gameObject.transform.position.x +.1){
+			gameObject.transform.Translate(new Vector3(speed*Time.deltaTime,0));
+		}else if (go.transform.position.x < gameObject.transform.position.x - .1){
+			gameObject.transform.Translate(new Vector3(-speed*Time.deltaTime,0));
+		}else if (go.transform.position.y > gameObject.transform.position.y + .1){
+			gameObject.transform.Translate(new Vector3(0,speed*Time.deltaTime));
+		}else if (go.transform.position.y < gameObject.transform.position.y - .1){
+			gameObject.transform.Translate(new Vector3(0,-speed*Time.deltaTime));
+		}
+	}
+}
