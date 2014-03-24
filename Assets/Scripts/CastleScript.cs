9f1cd30da40ea3e75e6af27b37bc1a2f50b49c62
niftybottle <niftybottle@gmail.com>
2014-03-23 23:00:33 -0400

    Popu-up using z key

--- Assets/Scripts/CastleScript.cs
+++ Assets/Scripts/CastleScript.cs
@@ -2,14 +2,15 @@
 using System.Collections;
 
 public class CastleScript : MonoBehaviour {
-
+	public GUIContolScript gcs;
 	// Use this for initialization
 	void Start () {
 	
 	}
-	
 	// Update is called once per frame
 	void Update () {
-	
+		if(Input.GetKeyDown(KeyCode.Z)){
+			gcs.go = true;
+		}
 	}
 }
